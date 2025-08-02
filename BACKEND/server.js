e83da38bb7184e46304687395c3e8aaf94007ed7
DevDre <bighalo77@gmail.com>
2025-08-02 20:05:35 +0100

    initial commit

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -31,7 +31,7 @@ app.get("/", (req, res) => {
 })
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
